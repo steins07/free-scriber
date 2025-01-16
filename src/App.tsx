b6513c5dfd8857aea0c88a7bd8da5cf58e70a69d
Romus Shrestha <sromus498@gmail.com>
2025-01-16 10:24:42 +0545

    updated  page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Homepage from "./components/Homepage"
+import FileDisplay from "./components/FileDisplay"
+
 
 function App() {
+  const [file, setFile] = useState<File|null>(null)
+  const [audioStream, setAudioStream] = useState<MediaStream|null>(null)
+
+  const isAudioAvailable = file || audioStream;
+  function handleAudioReset() {
+    setFile(null);
+    setAudioStream(null);
+  }
+
   return (
     <>
-      <div>hello</div>
+      <div className="flex flex-col mx-auto w-full max-w-[1000px]">
+        <section className="min-h-screen flex flex-col ">
+          <Header />
+          {isAudioAvailable ? (
+            <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream} />
+          ) : (
+            <Homepage setFile={setFile} setAudioStream={setAudioStream} />
+          )}
+        </section>
+        <Footer />
+      </div>
     </>
   )
 }
