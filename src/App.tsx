002cd5287170613ceee08fa3ca7d643d06000471
Romus Shrestha <sromus498@gmail.com>
2025-01-16 15:07:00 +0545

    updated component layout in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Homepage from "./components/Homepage"
 import FileDisplay from "./components/FileDisplay"
+import Information from "./components/Information"
+import Transcribing from "./components/Transcribing"
 
 
 function App() {
-  const [file, setFile] = useState<File | null>(null)
-  const [audioStream, setAudioStream] = useState<Blob | null>(null)
+  const [file, setFile] = useState<File | null>(null);
+  const [audioStream, setAudioStream] = useState<Blob | null>(null);
+  // const[output,setOutput] = useState<Blob | null>(null);
+  const [output, setOutput] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [finished,setFinished] = useState<boolean>(false);
 
   const isAudioAvailable = file || audioStream;
 
+  const worker= useRef<Worker | null>(null);
+
   useEffect(() => {
-    console.log(audioStream);
-  }, [audioStream])
+    
+  });
+  
   function handleAudioReset() {
     setFile(null);
     setAudioStream(null);
@@ -24,11 +33,16 @@ function App() {
       <div className="flex flex-col mx-auto w-full max-w-[1000px]">
         <section className="min-h-screen flex flex-col ">
           <Header />
-          {isAudioAvailable ? (
-            <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream} />
-          ) : (
-            <Homepage setFile={setFile} setAudioStream={setAudioStream} />
-          )}
+          {output ? (
+            <Information />) : (
+            isLoading ? (
+              <Transcribing />) : (
+              isAudioAvailable ? (
+                <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream} />
+              ) : (
+                <Homepage setFile={setFile} setAudioStream={setAudioStream} />
+              )))}
+          { }
         </section>
         <Footer />
       </div>
