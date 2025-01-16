afc38365da38e615a61ec436990419969b48409f
Romus Shrestha <sromus498@gmail.com>
2025-01-16 12:27:03 +0545

    minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
                     Scriber
                 </span>
             </h1>
-            <button className="flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-blue-400">
+            <button className="flex items-center gap-2 text-sm specialBtn px-3 py-2 rounded-lg text-blue-400">
                 <p>New</p>
                 <i className="fa-solid fa-plus"></i>
             </button>
