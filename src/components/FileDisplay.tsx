7e8422337ec9d6bf69a8e7e900d397508a6b88fd
Romus Shrestha <sromus498@gmail.com>
2025-01-16 12:26:45 +0545

    changed types and minor tailwind class

--- src/components/FileDisplay.tsx
+++ src/components/FileDisplay.tsx
@@ -2,28 +2,29 @@
 interface IFileDisplayPropType {
     handleAudioReset: () => void;
     file: File | null;
-    audioStream: MediaStream | null;
+    audioStream: Blob | null;
 }
 export default function FileDisplay({ handleAudioReset, file, audioStream }:IFileDisplayPropType) {
     return (
         <main
-            className="flex-1 flex flex-col justify-center p-4 gap-3 sm:gap-4 md:gap-5 text-center pb-20 w-fit max-w-full mx-auto "
+            className="flex-1 flex flex-col justify-center p-4 gap-3 sm:gap-4  sm:w-96 text-center pb-20 w-72 max-w-full mx-auto"
         >
             <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Your <span className="text-blue-400 bold">File</span>
             </h1>
-            <div className="flex flex-col text-left mx-auto mb-4">
+            <div className="flex flex-col text-left my-4">
                 <h3 className="font-semibold">Name</h3>
-                <p>{file?.name}</p>
+                <p>{file? file?.name: 'Custom Audio'}</p>
             </div>
             <div
                 className="flex items-center justify-between gap-4"
             >
-                <button className="text-slate-400"
+                <button className="text-slate-400 hover:text-blue-600 duration-200"
                     onClick={handleAudioReset}
-                >Reset</button>_
-                <button className="specialBtn px-4 py-2 rounded-lg text-blue-400">
+                >Reset</button>
+                <button className="specialBtn px-3 py-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium">
                     <p>Transcribe</p>
+                    <i className="fa-solid fa-pen-nib"></i>
                 </button>
             </div>
         </main>
