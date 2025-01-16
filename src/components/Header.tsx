5741dfde08268e3b74568825af56a5e5b9ca2596
Romus Shrestha <sromus498@gmail.com>
2025-01-16 15:04:29 +0545

    minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,19 @@
-
 export default function Header() {
-  return (
-    <header className="flex items-center justify-between gap-4 p-4">
-            <h1
-            className="font-medium"
-            >Free
-                <span className="text-blue-400 bold">
-                    Scriber
-                </span>
-            </h1>
-            <button className="flex items-center gap-2 text-sm specialBtn px-3 py-2 rounded-lg text-blue-400">
+    return (
+        <header className="flex items-center justify-between gap-4 p-4">
+            <a href="/" >
+                <h1
+                    className="font-medium"
+                >Free
+                    <span className="text-blue-400 bold">
+                        Scriber
+                    </span>
+                </h1>
+            </a>
+            <a href="/" className="flex items-center gap-2 text-sm specialBtn px-3 py-2 rounded-lg text-blue-400">
                 <p>New</p>
                 <i className="fa-solid fa-plus"></i>
-            </button>
+            </a>
         </header>
-  )
+    )
 }
