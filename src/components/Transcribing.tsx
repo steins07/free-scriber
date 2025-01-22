0ab8e0a46001f1e5868afc75c2ad78737cc3bbbf
Romus Shrestha <sromus498@gmail.com>
2025-01-22 10:17:03 +0545

    minor tailwind change

--- src/components/Transcribing.tsx
+++ src/components/Transcribing.tsx
@@ -8,7 +8,7 @@ export default function Transcribing(props) {
                 </h1>
                 <p>{!downloading ? 'warming up cylinders' : 'core cylinder engaged'}</p>
             </div>
-            <div className="flex flex-col gap-2 sm:gap-4 max-w-[400px] mx-auto w-full">
+            <div className="flex flex-col gap-2 sm:gap-3 max-w-[400px] mx-auto w-full">
                 {[0, 1, 2].map(val => {
                     return (
                         <div key={val}
