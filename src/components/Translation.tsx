b79da2ab94c3897e01c30aed294b8d1f1a08b955
Romus Shrestha <sromus498@gmail.com>
2025-01-23 14:02:57 +0545

    created component to display translation output

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,7 +1,46 @@
+import { LANGUAGES } from "../utils/presets"
 
-function Translation() {
+function Translation(props) {
+  const {
+    textElement,
+    translating,
+    toLanguage,
+    setToLanguage,
+    generateTranslation } = props
   return (
-    <div>Translation</div>
+    <div className="flex flex-col gap-2 max-w-[400px] w-full mx-auto">
+      {!translating && (<div className="flex flex-col gap-1 ">
+        <p className="text-xs sm:text-sm font-medium text-slate-500 mr-auto">
+          To Language
+        </p>
+        <div className="flex items-stretch gap-2">
+          <select className="flex-1 outline-none bg-white border border-solid border-transparent hover:border-blue-300 duration-200 focus:outline-none p-2 rounded" value={toLanguage} onChange={(e) => setToLanguage(e.target.value)}>
+            <option value={'Select Language'}>
+              Select Language
+            </option>
+            {
+              Object.entries(LANGUAGES).map((key, value) => {
+                return (
+                  <option key={value} value={key[1]}>
+                    {key[0]}
+                  </option>
+                )
+              })
+            }
+          </select>
+          <button className="specialBtn px-3 py-2 rounded-lg text-blue-400 hover:text-blue-600 duration-200"
+            onClick={generateTranslation}>Translate</button>
+        </div>
+      </div>)}
+      {textElement && !translating && (
+        <p>{textElement}</p>
+      )}
+      {translating && (
+        <div className="grid place-items-center">
+          <i className="fa-solid fa-spinner animate-spin"></i>
+        </div>
+      )}
+    </div>
   )
 }
 
