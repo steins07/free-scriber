de0f2b2bb94a95e8e009b8e38c0c666ad7d0099e
Romus Shrestha <sromus498@gmail.com>
2025-01-24 12:16:32 +0545

    changed layout

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,48 +1,52 @@
-import { ITranslationType } from "../types/allTypes"
-import { LANGUAGES } from "../utils/presets"
+import { ITranslationType } from "../types/allTypes";
+import { LANGUAGES } from "../utils/presets";
 
 function Translation({
   textElement,
-  translating,
   toLanguage,
+  translating,
   setToLanguage,
-  generateTranslation }: ITranslationType) {
- 
+  generateTranslation,
+}: ITranslationType) {
   return (
-    <div className="flex flex-col gap-2 max-w-[400px] w-full mx-auto">
-      {!translating && (<div className="flex flex-col gap-1 ">
-        <p className="text-xs sm:text-sm font-medium text-slate-500 mr-auto">
-          To Language
-        </p>
-        <div className="flex items-stretch gap-2">
-          <select className="flex-1 outline-none bg-white border border-solid border-transparent hover:border-blue-300 duration-200 focus:outline-none p-2 rounded" value={toLanguage} onChange={(e) => setToLanguage(e.target.value)}>
-            <option value={'Select Language'}>
-              Select Language
-            </option>
-            {
-              Object.entries(LANGUAGES).map((key, value) => {
+    <div className="flex flex-col gap-2 max-w-[400px] mx-auto w-full">
+      {!translating && (
+        <div className="flex flex-col gap-1">
+          <p className="text-xs sm:text-sm font-medium text-slate-500 mr-auto">
+            To Language
+          </p>
+          <div className="flex flex-col sm:flex-row items-stretch gap-2 sm:max-w-[100] ">
+            <select
+              className=" flex-1 outline-none bg-white focus:outline-none border border-solid border-transparent hover:border-blue-300 duration-200 p-2  rounded"
+              value={toLanguage}
+              onChange={(e) => setToLanguage(e.target.value)}
+            >
+              <option value={"Select Language"}>Select Language</option>
+              {Object.entries(LANGUAGES).map(([key, val]) => {
                 return (
-                  <option key={value} value={key[1]}>
-                    {key[0]}
+                  <option key={key} value={val}>
+                    {key}
                   </option>
-                )
-              })
-            }
-          </select>
-          <button className="specialBtn px-3 py-2 rounded-lg text-blue-400 hover:text-blue-600 duration-200"
-            onClick={generateTranslation}>Translate</button>
+                );
+              })}
+            </select>
+            <button
+              onClick={generateTranslation}
+              className="specialBtn px-3 py-2 rounded-lg text-blue-400 hover:text-blue-600 duration-200 mx-auto"
+            >
+              Translate
+            </button>
+          </div>
         </div>
-      </div>)}
-      {textElement && !translating && (
-        <p>{textElement}</p>
       )}
+      {textElement && !translating && <p>{textElement}</p>}
       {translating && (
         <div className="grid place-items-center">
           <i className="fa-solid fa-spinner animate-spin"></i>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Translation
+export default Translation;
