d52e2bc8cd571481cdefacb7bfe88cb2503c91fa
Romus Shrestha <sromus498@gmail.com>
2025-01-24 11:29:55 +0545

    minor changes

--- src/utils/translate.worker.ts
+++ src/utils/translate.worker.ts
@@ -1,42 +1,47 @@
-import { pipeline,env } from "@xenova/transformers";
+import { pipeline, env, PipelineType } from "@xenova/transformers";
 
-env.allowLocalModels=false;
-class MyTranslationPipeline{
-    static task: PipelineType = 'translation';
-        static model = "Xenova/nllb-200-distilled-600M";
-        static instance: any = null;
-    
-        static async getInstance(
-            progress_callback: (data: ProgressCallbackData) => void
-        ): Promise<any> {
-            if (this.instance === null) {
-                this.instance = await pipeline(this.task, this.model, { progress_callback });
-            }
-            return this.instance;
+env.allowLocalModels = false;
+class MyTranslationPipeline {
+    static task: PipelineType = "translation";
+    static model = "Xenova/nllb-200-distilled-600M";
+    static instance: any = null;
+
+    static async getInstance(progress_callback: (progress: any) => void) {
+        if (this.instance === null) {
+            this.instance = await pipeline(this.task, this.model, {
+                progress_callback,
+            });
         }
+        return this.instance;
+    }
 }
 
-self.addEventListener('message',async (event: MessageEvent) => {
-    let translator= await MyTranslationPipeline.getInstance(
-        x=>{
-            self.postMessage(x)
-        }
-    )
-    let output= await translator(event.data.text,{
-        tgt_lang:event.data.tgt_language,
-        src_lang:event.data.src_language,
-        callback_function: x=>{
+self.addEventListener("message", async (event: MessageEvent) => {
+    // Define the structure of the event data
+    const data = event.data as {
+        text: string;
+        tgt_language: string;
+        src_language: string;
+    };
+
+    let translator = await MyTranslationPipeline.getInstance((x) => {
+        self.postMessage(x);
+    });
+    let output = await translator(event.data.text, {
+        tgt_lang: data.tgt_language,
+        src_lang: data.src_language,
+        callback_function: (x: any) => {
             self.postMessage({
-                status:'update',
-                output:translator.tokenizer.decode(x[0].output_token_ids,{
-                    skip_special_tokens:true
-                })
-            })
-        }
-    })
+                status: "update",
+                output: translator.tokenizer.decode(x[0].output_token_ids, {
+                    skip_special_tokens: true,
+                }),
+            });
+        },
+    });
 
     self.postMessage({
-        status:'complete',
-        output
-    })
-})
+        status: "complete",
+        output,
+    });
+});
